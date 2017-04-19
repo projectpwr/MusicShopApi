ea0d5d7bc72dc739b0fe59517d6349fd781c3173
Will Young <willyoung0@googlemail.com>
2017-04-19 21:15:58 +0100

    new Models.Tests .net core class library for xUnit unit tests

---
+++ tests/Models.Tests/Class1Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+using Models;
+
+namespace Models.Tests
+{
+    public class Class1Test
+    {
+        [Fact]
+        public void someMethodShouldDoSomething()
+        {
+        }
+
+        [Fact]
+        public void someMethodShouldDoSomething222()
+        {
+            var thing = new Class1();
+            var expected = "Oh hi there";
+            var actual = thing.something();
+            Assert.Equal(expected, actual);
+        }
+    }
+}
