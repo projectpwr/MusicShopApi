6153a1dfe35b9bf0e1ee775214ce7293d97c4dfe
Will Young <willyoung0@googlemail.com>
2017-05-09 22:44:47 +0100

    first crack at new repository interface, along with basic entity interface to ensure all entities have ids and rowVersions

---
+++ DataAccessInterfaces/Entities/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessInterfaces.Entities
+{
+    interface IEntity
+    {
+        int Id { get; set; }
+        byte[] RowVersion { get; set; }
+    }
+}
---
+++ DataAccessInterfaces/Repositories/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessInterfaces.Data;
+using DataAccessInterfaces.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace DataAccessInterfaces.Repositories
+{
+    interface IRepository<T> where T : IEntity
+    {
+  
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Save();
+        T Get(int Id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> GetBy(Expression<Func<T, bool>> predicate);
+    }
+}
