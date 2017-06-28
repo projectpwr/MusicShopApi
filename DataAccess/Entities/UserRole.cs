fdc3b00755d985b66725dceea7909214ac56ed72
Will Young <willyoung0@googlemail.com>
2017-06-28 22:44:04 +0100

    user role class

---
+++ DataAccess/Entities/UserRole.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Entities
+{
+    public class UserRole : IdentityRole
+    {
+    }
+}
