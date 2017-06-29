6e6c255f6f38b6e2e819f8f2c3d1c96045f61c67
Will Young <willyoung0@googlemail.com>
2017-06-29 22:36:53 +0100

    abstract base class for controllers to extend controller base but also allow common controller methods to be shared across the board

---
+++ Api/Controllers/DomainControllerBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    public abstract class DomainControllerBase : ControllerBase
+    {
+        public string SerializeObject(Object target)
+        {
+            return JsonConvert.SerializeObject(target, Formatting.Indented);
+        }
+
+    }
+}
