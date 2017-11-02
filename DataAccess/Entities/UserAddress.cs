31b62f518bbc67bff67caaa21ee4818a3d75697d
Will Young <willyoung0@googlemail.com>
2017-11-02 22:33:15 +0000

    user address class back in here however still doesnt seem to be getting picked up by entity framework

---
+++ DataAccess/Entities/UserAddress.cs
@@ -0,0 +1,28 @@
+﻿using DataAccessInterfaces.Entities;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataAccess.Entities
+{
+    public class UserAddress : IEntity
+    {
+        [Key]
+        [JsonProperty(Required = Required.Default)]
+        public int Id { get; private set; }
+
+        [Timestamp]
+        [JsonProperty(Required = Required.Default)]
+        public byte[] RowVersion { get; set; }
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+        public int AddressId { get; set; }
+        public Address Address { get; set; }
+
+        private UserAddress() { }
+    }
+}
